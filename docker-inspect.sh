79ed5ac09f8689dbb7c8528651783b4a73ad37f3
alwani achmad <alwaniachmad7031999aa@gmail.com>
2023-10-30 15:56:09 +0700

    Inspect update

--- docker-inspect.sh
+++ docker-inspect.sh
@@ -1,1 +1,15 @@
 docker image inspect mongo:latest
+
+docker container inspect 533
+
+docker volume inspect mongodata
+
+docker network inspect mongonetwork
+
+
+###
+Untuk melihat detail dari image, gunakan : docker image inspect namaimage
+Untuk melihat detail dari container, gunakan : docker container inspect namacontainer
+Untuk melihat detail dari volume, gunakan : docker volume inspect namavolume
+Untuk melihat detail dari network, gunakan : docker network inspect namanetwork
+###
